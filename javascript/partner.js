26948d90bb79e97d60c578420da5df35af349eaf
luozhiyuan <810811031@qq.com>
2021-11-02 23:37:16 +0800

    1

--- javascript/partner.js
+++ javascript/partner.js
@@ -37,22 +37,22 @@ var initPcCharts = function () {
         ],
         yAxis: [
             {
-            type: 'value',
-            name: '金额（元）',
-            min: 0,
-            max: 10000,
-            axisLabel: {
-                formatter: '{value} 元'
-            }
+                type: 'value',
+                name: '金额（元）',
+                min: 0,
+                max: 10000,
+                axisLabel: {
+                    formatter: '{value} 元'
+                }
             },
             {
-            type: 'value',
-            name: '收益率（%）',
-            min: 0,
-            max: 100,
-            axisLabel: {
-                formatter: '{value} %'
-            }
+                type: 'value',
+                name: '收益率（%）',
+                min: 0,
+                max: 100,
+                axisLabel: {
+                    formatter: '{value} %'
+                }
             }
         ],
         series: [
@@ -100,11 +100,20 @@ var initPcCharts = function () {
             name: '收益率',
             type: 'line',
             smooth: true,
+            areaStyle: {},
             itemStyle: {
                 normal: {
-                    color: 'rgb(255, 131, 110)',
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1,[
+                        {
+                            offset: 0, color: 'rgba(255, 131, 110, .31)' // 0% 处的颜色
+                        }, 
+                        {
+                            offset: 1, color: 'rgba(183, 215, 253, 0)' // 100% 处的颜色
+                        }
+                    ]
+                    ),
                     lineStyle: {
-                        color: 'rgb(255, 131, 110)'
+                        color: 'rgba(255, 131, 110, .4)'
                     }
                 }
             },
@@ -120,7 +129,130 @@ var initPcCharts = function () {
 
 
 var initMoblieCharts = function () {
-    
+    var charts = echarts.init(document.getElementById('charts-mobile'))
+    var option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross',
+                crossStyle: {
+                    color: '#999'
+                }
+            }
+        },
+        grid: {
+            top: '8%',
+            left: '4%',
+            right: '10%',
+            bottom: '15%',
+            containLabel: true
+        },
+        legend: {
+            x: 'center',
+            y: 'bottom',
+            padding:[0, 0, 30, 0],
+            data: ['成本费用', '收入', '收益率']
+        },
+        xAxis: [
+            {
+                type: 'value',
+                name: '',
+                min: 0,
+                max: 10000,
+                axisLabel: {
+                    formatter: '{value} 元'
+                }
+            },
+            {
+                type: 'value',
+                name: '',
+                min: 0,
+                max: 100,
+                axisLabel: {
+                    formatter: '{value} %'
+                }
+            }
+        ],
+        yAxis: {
+            type: 'category',
+            inverse: true,
+            axisLine: {
+                lineStyle: {
+                    color: '#999',
+                    width: 1
+                }
+            },
+            data:  ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        },
+        series: [
+            {
+                name: '成本费用',
+                type: 'bar',
+                barWidth : 10,
+                data: [
+                    4000, 4000, 4000, 6200, 6200, 5000, 5000, 6000, 6200, 5800, 5600, 5400
+                ],
+                itemStyle: {
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(1, 0, 0, 0, [{
+                            offset: 0,
+                            color: '#4188F3'
+                        }, {
+                            offset: 1,
+                            color: '#fff'
+                        }]),
+                        barBorderRadius: [0, 5, 5, 0]
+                    }
+                }
+            },
+            {
+                name: '收入',
+                type: 'bar',
+                barWidth : 10,
+                data: [
+                    3000, 3000, 3000, 3000, 4200, 4200, 4200, 4200, 7000, 7200, 7000, 8500
+                ],
+                itemStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(1, 0, 0, 0, [{
+                                offset: 0,
+                                color: '#FDB32A'
+                            }, {
+                                offset: 1,
+                                color: '#fff'
+                            }]),
+                            barBorderRadius: [0, 5, 5, 0]
+                        }
+                }
+                },
+                {
+                name: '收益率',
+                type: 'line',
+                smooth: true,
+                areaStyle: {},
+                itemStyle: {
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(1, 0, 0, 0,[
+                            {
+                                offset: 0, color: 'rgba(255, 131, 110, .31)' // 0% 处的颜色
+                            }, 
+                            {
+                                offset: 1, color: 'rgba(183, 215, 253, 0)' // 100% 处的颜色
+                            }
+                        ]
+                        ),
+                        lineStyle: {
+                            color: 'rgba(255, 131, 110, .4)'
+                        }
+                    }
+                },
+                xAxisIndex: 1,
+                data: [25, 40, 45, 50, 60, 70, 70, 70, 75, 80, 85, 85]
+            }
+        ]
+    }
+    if (charts) charts.resize()
+    charts.setOption(option, true)
 }
 
 
@@ -131,3 +263,12 @@ if (window._config_.isMoblie) {
         window._config_.map.initPcCharts = initPcCharts
     }
 }
+
+$('.reset').click(function () {
+    $('.charts-limit').fadeOut()
+})
+
+$('.btn').click(function () {
+    $('.charts-limit').fadeIn()
+    initMoblieCharts()
+})
