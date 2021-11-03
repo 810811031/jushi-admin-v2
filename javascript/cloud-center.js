411c1092963de6c1dbec0e0350e99c54b6f0708c
luozhiyuan <810811031@qq.com>
2021-11-03 21:56:32 +0800

    1

--- javascript/cloud-center.js
+++ javascript/cloud-center.js
@@ -1,15 +1,17 @@
-$('.box').mouseenter(function () {
-    $('.box').css({ width: '20%' })
-    $(this).css({ width: '40%' })
-    $(this).find('.title').css({ fontSize: '' })
-    $(this).find('.description').css({ fontSize: '' })
-    $(this).find('img').css({ transform: 'translateX(-50%) scale(1.1, 1.1)' })
-})
-
-$('.box').mouseleave(function () {
-    $('.box').css({ width: '25%' })
-    $(this).find('.title').css({ fontSize: '1.25vw' })
-    $(this).find('.description').css({ fontSize: '0.729vw' })
-    $(this).find('img').css({ transform: 'translateX(-50%)' })
-})
+if (!window._config_.isMoblie) {
+    $('.box').mouseenter(function () {
+        $('.box').css({ width: '20%' })
+        $(this).css({ width: '40%' })
+        $(this).find('.title').css({ fontSize: ' 1.771vw' })
+        $(this).find('.description').css({ fontSize: '1.042vw' })
+        $(this).find('img').css({ transform: 'translateX(-50%) scale(1.1, 1.1)' })
+    })
+    
+    $('.box').mouseleave(function () {
+        $('.box').css({ width: '25%' })
+        $(this).find('.title').css({ fontSize: '1.25vw' })
+        $(this).find('.description').css({ fontSize: '0.729vw' })
+        $(this).find('img').css({ transform: 'translateX(-50%)' })
+    })
+}
 
