04bf78aa1acc6e30d4a2a23dedabecf7485ba5be
luozhiyuan <810811031@qq.com>
2021-11-03 19:51:19 +0800

    1

--- javascript/cloud-center.js
+++ javascript/cloud-center.js
@@ -1,11 +1,15 @@
 $('.box').mouseenter(function () {
     $('.box').css({ width: '20%' })
     $(this).css({ width: '40%' })
+    $(this).find('.title').css({ fontSize: '' })
+    $(this).find('.description').css({ fontSize: '' })
     $(this).find('img').css({ transform: 'translateX(-50%) scale(1.1, 1.1)' })
 })
 
 $('.box').mouseleave(function () {
     $('.box').css({ width: '25%' })
+    $(this).find('.title').css({ fontSize: '1.25vw' })
+    $(this).find('.description').css({ fontSize: '0.729vw' })
     $(this).find('img').css({ transform: 'translateX(-50%)' })
 })
 
